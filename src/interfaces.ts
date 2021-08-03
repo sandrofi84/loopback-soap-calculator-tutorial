54efc28f2c60941dec8223cb47f549d7b7b7a784
Sandro <sandrofillinich@gmail.com>
2021-08-03 12:17:44 +0100

    feat: create service for app

---
+++ src/interfaces.ts
@@ -0,0 +1,24 @@
+export interface MultiplyResponse {
+  result: {
+    value: number;
+  };
+}
+export interface AddResponse {
+  result: {
+    value: number;
+  };
+}
+export interface SubtractResponse {
+  result: {
+    value: number;
+  };
+}
+export interface DivideResponse {
+  result: {
+    value: number;
+  };
+}
+export interface CalculatorParameters {
+  intA: number;
+  intB: number;
+}
---
+++ src/services/calculator.service.ts
@@ -0,0 +1,26 @@
+import {inject, Provider} from '@loopback/core';
+import {getService} from '@loopback/service-proxy';
+import {CalculatorDataSource} from '../datasources';
+import {AddResponse, CalculatorParameters, DivideResponse, MultiplyResponse, SubtractResponse} from '../interfaces';
+
+export interface Calculator {
+  // this is where you define the Node.js methods that will be
+  // mapped to REST/SOAP/gRPC operations as stated in the datasource
+  // json file.
+  multiply(args: CalculatorParameters): Promise<MultiplyResponse>;
+  add(args: CalculatorParameters): Promise<AddResponse>;
+  divide(args: CalculatorParameters): Promise<DivideResponse>;
+  subtract(args: CalculatorParameters): Promise<SubtractResponse>;
+}
+
+export class CalculatorProvider implements Provider<Calculator> {
+  constructor(
+    // calculator must match the name property in the datasource json file
+    @inject('datasources.calculator')
+    protected dataSource: CalculatorDataSource = new CalculatorDataSource(),
+  ) {}
+
+  value(): Promise<Calculator> {
+    return getService(this.dataSource);
+  }
+}
